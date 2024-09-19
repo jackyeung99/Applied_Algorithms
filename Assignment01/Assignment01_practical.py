72b594b4118aa28424953ab04831c824cd9ad793
jackyeung99 <jackyeung99@gmail.com>
2024-09-19 13:46:59 -0400

    BUILD: almost done, help elara needs work

--- Assignment01/Assignment01_practical.py
+++ Assignment01/Assignment01_practical.py
@@ -1,49 +1,113 @@
 from typing import List, Tuple, Union
+import heapq 
 
-
-
+# ============ Q1 ============
 def processOperations(n: int, operations: List[Tuple[int, Union[str, Tuple[str, int]]]]): 
-    pass
+    output = []
+    #keep movie name, rating, access count, and time
+    movie_info = {}
+  
+    
+    for idx, i in enumerate(operations):
 
+        # check if in
+        if i[0] == 1:
+            movie = i[1]
+
+            if movie in movie_info:
+            
+                output.append(movie_info[movie][0])
+                movie_info[movie][1] += 1
+       
+            else:
+                output.append(-1)             
+            
+        #update hard drive
+        else:
+            new_movie, rating = i[1]
+            
+            if new_movie in movie_info:
+                movie_info[new_movie][0] = rating
+                movie_info[new_movie][1] += 1
+    
+            else:
+
+                if len(movie_info) >= n:
+                    lowest_movie = min(movie_info, key = lambda x: (movie_info[x][1], movie_info[x][2]))
+                    del movie_info[lowest_movie]
+
+                movie_info[new_movie] = [rating, 1, idx]
+
+    
+    return output
+
+
+
+# ============ Q3 ============
 def combine_signals(signals):
     return [(signals[i-1] | signals[i]) for i in range(1, len(signals))]
 
-
+# ============ Q4 ============
 def numberOfTeams(ratings:List[int])->int:
 
-    teams = 0
-    for i in range(len(ratings)):
+    #brute force
+    # teams = 0
+    # for i in range(len(ratings)):
         
-        for j in range(i + 1 , len(ratings)):
+    #     for j in range(i + 1 , len(ratings)):
             
-            for k in range(j + 1, len(ratings)):
+    #         for k in range(j + 1, len(ratings)):
 
-                if (ratings[i] < ratings[j] < ratings[k]) or (ratings[i] > ratings[j] > ratings[k]):
-                    teams += 1
+    #             if (ratings[i] < ratings[j] < ratings[k]) or (ratings[i] > ratings[j] > ratings[k]):
+    #                 teams += 1
 
-    return teams
-    
+    # return teams
 
+    team_count = 0
+    for j in range(len(ratings)):
+        
+        left_less = 0
+        left_greater = 0
+        right_less = 0
+        right_greater = 0
 
+        for i in range(j):
+            if ratings[i] < ratings[j]:
+                left_less += 1
+            elif ratings[i] > ratings[j]:
+                left_greater += 1
+
+        for k in range(j+1, len(ratings)):
+            if ratings[k] < ratings[j]:
+                right_less += 1
+            elif ratings[k] > ratings[j]:
+                right_greater += 1
+
+        team_count += (left_less * right_greater) + (left_greater * right_less)
+
+    return team_count
+
+# ============ Q5 ============
 def findPowerLevels(powerLevels: list[int]) -> list[int]:
-    pass
-    # n = len(powerLevels)
-    # newPower = [1] * n
+    final_powerLevels = [1] * len(powerLevels)
 
-    # left_product = 1
-    # for i in range(n):
-    #     newPower[i] = left_product
-    #     left_product *= powerLevels[i]
+    left_product = 1
+    for i in range(len(powerLevels)):
+        final_powerLevels[i] = left_product
+        left_product *= powerLevels[i]
 
-    # right_product = 1
-    # for i in range(n-1, -1, -1):
-    #     newPower[i] *= right_product
-    #     right_product *= powerLevels[i]
+    right_product = 1
+    for i in range(len(powerLevels)-1, -1, -1):
+        final_powerLevels[i] *= right_product
+        right_product *= powerLevels[i]
 
-    # return newPower
+    return final_powerLevels
 
-
+# ============ Q6 ============
 def decrypt(s:str)->int:
+    if len(s) % 2 == 1:
+        return -1 
+    
     seen1 = {}
     seen2 = {}    
 
@@ -74,12 +138,35 @@ def decrypt(s:str)->int:
     
     return changes_needed
 
-
+# ============ Q7 ============
 def magicalScribe(n: int) -> str: 
+    if n == 1:
+        return '1'
+    
 
-    pass
+    def decode_sequence(sequence):
+        end_sequence = []
+        count = 1
+        
+        for i in range(1, len(sequence)):
 
+            if sequence[i] == sequence[i-1]:
+                count += 1
+            else:
+                end_sequence.append(f"{count}{sequence[i-1]}")
+                count = 1
 
+        end_sequence.append(f"{count}{sequence[-1]}")
+        return ''.join(end_sequence)
+    
+
+    str = '1'
+    for i in range(1,n):
+        str = decode_sequence(str)
+      
+    return str
+
+# ============ Q8 ============
 def sortVowels(code: str) -> str:
     str_vowels = []
     vowels = ['a', 'e', 'i', 'o', 'u']
@@ -96,3 +183,70 @@ def sortVowels(code: str) -> str:
             result.append(char)
 
     return ''.join(result)
+
+# ============ Q9 ============
+def helpElara(words: List[str], width: int) -> List[str]:
+    results = []
+    line = []
+    buffer_length = 0
+    for word in words[:-1]:
+        print(word, len(word))
+        if len(word) + 1 + buffer_length <= width:
+            line.append(word)
+            buffer_length += len(word) + 1
+        else:
+
+            filled_line = fill_spaces(line, width)
+            results.append(filled_line)
+            line = [word]
+            buffer_length = len(word) +1 
+
+    #clear last line   
+    filled_line = fill_spaces(line, width)
+    results.append(filled_line)
+    # add last word
+    final_word = words[-1] 
+    filled_last = fill_spaces([final_word], width) 
+    results.append(filled_last)
+
+    print([len(x) for x in results])
+    return results
+
+def fill_spaces(line, width):
+
+    word_lengths = sum([len(x) for x in line])
+    spaces_needed = width - word_lengths
+    gaps = len(line) -1 
+
+    if gaps == 0:
+        return ' '.join(line).ljust(width)
+    
+   
+    spaces_per_gap = spaces_needed // gaps
+    #left over spaces
+    extra_spaces = spaces_needed % gaps
+
+    result = ""
+    for i in range(gaps):
+        result += line[i] + ' ' * (spaces_per_gap + (1 if i < extra_spaces else 0))
+     
+    result += line[-1]
+  
+    return result
+
+# ============ Q10 ============
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+def findTreasure(head):
+    fast = head
+    while fast and fast.next: 
+        head = head.next
+        fast = fast.next.next
+
+        if head is fast:
+            return False
+
+    return True
--- Assignment01/Assignment01_test.py
+++ Assignment01/Assignment01_test.py
@@ -3,6 +3,40 @@
 import pytest 
 from Assignment01_practical import *
 
+def test_q1():
+
+    n = 2
+    operations = [
+        (1, "GOT"), # Check if "GOT" is present
+        (2, ("GOT", 9)), # Add "GOT" with rating 9
+        (1, "GOT"), # Check if "GOT" is present
+        (2, ("NARUT", 10)), # Add "NARUT" with rating 10
+        (1, "NARUT"), # Check if "NARUT" is present
+        (2, ("BARUT", 6)), # Add "BARUT" with rating 6
+        (1, "GOT"), # Check if "GOT" is present
+        (1, "BARUT"), # Check if "BARUT" is present
+        ]
+    
+    test_output = [-1, 9, 10, -1, 6]
+    
+    assert processOperations(n, operations) == test_output
+
+
+    n_2 = 2
+    operations_2 = [
+        (2, ("GOT", 9)), # Add "GOT" with rating 9
+        (2, ("NARUT", 10)), # Add "NARUT" with rating 8
+        (1, "GOT"), # Check if "GOT" is present (increases access count)
+        (2, ("BARUT", 6)), # Add "BARUT" with rating 6
+        (1, "NARUT"), # Check if "NARUT" is present
+        (2, ("GOT", 7)), # Update rating of "GOT" (increases access count)
+        (2, ("BBAD", 9)), # Add "BBAD" with rating 9
+        (1, "BARUT"), # Check if "BARUT" is present
+        (1, "GOT"), # Check if "GOT" is present
+        (1, "BBAD"), # Check if "BBAD" is present
+        ]
+    test_output_2 = [9, -1, -1, 7, 9]
+    assert processOperations(n_2, operations_2) == test_output_2
 
 def test_q3():
 
@@ -58,15 +92,20 @@ def test_q6():
 
 def test_q7():
 
-    input1 = 'aaabbb'
-    output1 = 3
+    input1 = 1
+    output1 = '1'
 
     assert magicalScribe(input1) == output1
 
-    input2 = 'xyyx'
-    output2 = 0
+    input2 = 5
+    output2 = '111221'
 
     assert magicalScribe(input2) == output2
+
+    input3 = 4
+    output3 = '1211'
+
+    assert magicalScribe(input3) == output3
 
 def test_q8():
 
@@ -79,3 +118,37 @@ def test_q8():
     output2 = 'bREcaweYnu'
 
     assert sortVowels(input2) == output2
+
+def test_q9():
+
+    # words = ["To","be","or","no","question","answer","is","question"]
+    # width = 13
+    # output = [
+    #         "To  be  or no",
+    #         "question     ",
+    #         "answer     is",
+    #         "question     "
+    #         ]
+    # assert helpElara(words, width) == output
+
+
+    words2 =  [
+        "Hello",
+        "world",
+        "this",
+        "is",
+        "a",
+        "test",
+        "of",
+        "help",
+        "Elara"
+    ],
+    width2 = 15  
+    output2 = [
+    "Hello     world",
+    "this  is a test",
+    "of help Elara  "
+    ]
+   
+
+    assert helpElara(words2, width2) == output2
